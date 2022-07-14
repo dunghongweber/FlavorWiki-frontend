41646b8a6bb7eb60d1278a3d88fc91bc7b75ccda
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2022-07-14 16:52:44 +0700

    layout maybe

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+/**
+ *  DUMMY FILE
+ * this could be used if the app is bigger and need specific layout for front end
+ * @returns DUMMY FILE
+ */
 export default function Layout() {
   return <div>Layout</div>;
 }
